3812237961f4048a732027314cca633ae7f8e907
Rubén Manzano Santacruz <rmanzanointecrobots@gmail.com>
2024-03-21 17:24:12 +0100

    filtrado

--- src/components/visits/VisitList.tsx
+++ src/components/visits/VisitList.tsx
@@ -11,13 +11,22 @@ const VisitList: React.FC<VisitListProps> = ({
   upcomingOnly = false,
   visits,
 }) => {
+  const currentDate = new Date(); // Obtener la fecha y hora actual del sistema
+
+  // Filtrar las visitas basadas en si son próximas o no
   const filteredVisits = upcomingOnly
-    ? visits.filter((visit) => new Date(visit.startDate) > new Date())
+    ? visits.filter((visit: any) => {
+        const visitStartDate = new Date(
+          `${visit.start_date}T${visit.start_time}`
+        );
+        // Comparar con la fecha y hora actual del sistema
+        return visitStartDate > currentDate;
+      })
     : visits;
 
   return (
     <div className="bg-black">
-      {filteredVisits.length === 0 ? ( // Verificar si la lista de visitas filtradas está vacía
+      {filteredVisits.length === 0 ? (
         <div className="text-white text-2xl font-bold text-center py-8">
           No hay visitas próximas
         </div>
