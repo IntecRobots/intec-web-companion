d699bc61ad54c89256e5f04e3bfbcb19fa60771d
Pedro Monzón <pedromonzon239@gmail.com>
2024-03-22 10:13:25 +0100

    Limpieza de codigo y desinstalacion de dependencias inutilizables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,73 +53,99 @@ const Home: React.FC = () => {
             />
           </div>
           <div className="w-1 bg-gray-700 h-3/4 self-center"></div>
-
-          {/* Imagen a la izquierda */}
-          <div className="flex-1 flex justify-center">
-            <Image
-              src="/images/placeholderbot2-removebg-preview.png"
-              alt="Left side image"
-              width={200} // Establece el ancho deseado
-              height={100} // Establece el alto deseado para que coincida con el formulario
-            />
-          </div>
-          {/* Imagen a la izquierda */}
-          <div className="flex-1 flex justify-center">
-            <Image
-              src="/images/placeholderbot2-removebg-preview.png"
-              alt="Left side image"
-              width={200} // Establece el ancho deseado
-              height={100} // Establece el alto deseado para que coincida con el formulario
-            />
-          </div>
-
-          <div className="w-1 bg-white-700 my-12"></div>
-          <div className="w-1 bg-white-700 my-12"></div>
-
-          {/* Formulario de inicio de sesión a la derecha */}
-          <div className="flex-1">
-            <div className="w-full max-w-sm mx-auto bg-gray-700 rounded-lg shadow-lg py-10 px-6">
-              {" "}
-              {/* Aumenté el padding vertical aquí */}
-              <LoginTitle />
-              <form
-                action={async (formData) => {
-                  "use server";
-                  await login(formData);
-                  redirect("/home");
-                }}
-              >
-                <LoginInput
-                  type="text"
-                  id="username"
-                  name="username"
-                  placeholder="Nombre de usuario..."
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                />
-                <LoginInput
-                  type="password"
-                  id="password"
-                  name="password"
-                  placeholder="Contraseña..."
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <LoginButton />
-              </form>
-              <SignUpLink />
+          <div className="flex min-h-screen items-center justify-between px-10">
+            <div className="absolute top-10 left-1/2 transform -translate-x-1/2">
+              <Image
+                src="/images/intecrobots_dark.png"
+                alt="logo"
+                width="200"
+                height="200"
+              />
             </div>
-          </div>
+            <div className="w-1 bg-gray-700 h-3/4 self-center"></div>
 
+            {/* Imagen a la izquierda */}
+            <div className="flex-1 flex justify-center">
+              <Image
+                src="/images/placeholderbot2-removebg-preview.png"
+                alt="Left side image"
+                width={200} // Establece el ancho deseado
+                height={100} // Establece el alto deseado para que coincida con el formulario
+              />
+            </div>
+            {/* Imagen a la izquierda */}
+            <div className="flex-1 flex justify-center">
+              <Image
+                src="/images/placeholderbot2-removebg-preview.png"
+                alt="Left side image"
+                width={200} // Establece el ancho deseado
+                height={100} // Establece el alto deseado para que coincida con el formulario
+              />
+            </div>
+
+            <div className="flex-1 flex justify-center">
+              <Image
+                src="/images/placeholderbot2-removebg-preview.png"
+                alt="Left side image"
+                width={200}
+                height={100}
+              />
+            </div>
+
+            <div className="w-1 bg-white-700 my-12"></div>
+            <div className="w-1 bg-white-700 my-12"></div>
+
+            {/* Formulario de inicio de sesión a la derecha */}
+            <div className="flex-1">
+              <div className="w-full max-w-sm mx-auto bg-gray-700 rounded-lg shadow-lg py-10 px-6">
+                {" "}
+                {/* Aumenté el padding vertical aquí */}
+                <LoginTitle />
+                <form
+                  action={async (formData) => {
+                    "use server";
+                    await login(formData);
+                    redirect("/home");
+                  }}
+                >
+                  <LoginInput
+                    type="text"
+                    id="username"
+                    name="username"
+                    placeholder="Nombre de usuario..."
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                  <LoginInput
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="Contraseña..."
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <LoginButton
+                    onClick={function (
+                      e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+                    ): void {
+                      throw new Error("Function not implemented.");
+                    }}
+                  />
+                </form>
+                <SignUpLink />
+              </div>
+            </div>
+
+            {/* Copi Right */}
+            <p className="text-xs text-gray-400 absolute bottom-4 left-1/2 transform -translate-x-1/2">
+              &copy; 2024 Intec Robots.
+            </p>
+          </div>
           {/* Copi Right */}
           <p className="text-xs text-gray-400 absolute bottom-4 left-1/2 transform -translate-x-1/2">
             &copy; 2024 Intec Robots.
           </p>
         </div>
-        {/* Copi Right */}
-        <p className="text-xs text-gray-400 absolute bottom-4 left-1/2 transform -translate-x-1/2">
-          &copy; 2024 Intec Robots.
-        </p>
       </div>
     </div>
   );
